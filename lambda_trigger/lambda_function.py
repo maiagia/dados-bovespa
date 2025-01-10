3e19280c519ffd09627f8bc95dd503665083a689
Kleryton Souza <kleryton.dev@gmail.com>
2025-01-10 08:47:10 -0300

    Adiciona função Lambda para iniciar job do Glue

---
+++ lambda_trigger/lambda_function.py
@@ -0,0 +1,12 @@
+import boto3
+import json
+
+glue_job_name = "etl-transformacao-dados-bovespa" 
+glue_client = boto3.client('glue')
+
+def lambda_handler(event, context):
+    try:
+        response = glue_client.start_job_run(JobName=glue_job_name)
+        print(f"Job {glue_job_name} iniciado com sucesso! JobRunId: {response['JobRunId']}")
+    except Exception as e:
+        print(f"Erro ao iniciar o Job Glue: {str(e)}")
